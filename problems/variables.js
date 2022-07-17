dc6dde7cf89a327951c4e5750ffcdb8bd10961fe
mohammad <mohammad0914@gmail.com>
2022-07-17 11:55:50 +0430

    init

--- problems/variables.js
+++ problems/variables.js
@@ -16,13 +16,23 @@
 //  * Calculate how many snacks you would eat total, from your current age until the maximum age.
 //  * Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
 
+let age = 37;
+let maxAge = 100;
+let snackAmount = 2;
+let snackNumber = (maxAge - age) * snackAmount;
+console.log(`You will need ${snackNumber} to last you until the age of ${maxAge}`);
+
 // ## Problem Three
 
-// Calculate properties of a circle, using the definitions: http://math2.org/math/geometry/circles.htm 
+// Calculate properties of a circle, using the definitions: http://math2.org/math/geometry/circles.htm
 //  * Store a radius into a variable.
 //  * Calculate the circumference based on the radius, and log "The circumference is `circumferenceResult`".
 //  * Calculate the area based on the radius, and log "The area is `areaOfCircle`".
 //  * Hint: https://www.w3schools.com/jsref/jsref_pi.asp
+let radius = 5;
+let circumferenceResult = 2 * Math.PI * radius;
+let areaOfCircle = Math.PI * radius * radius;
+
 
 // ## Problem Four
 
@@ -31,6 +41,14 @@
 //  * Convert it to fahrenheit and output "`tempInCelsius`°C is `tempInFahrenheit`°F".
 //  * Now store a fahrenheit temperature into a variable.
 //  * Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelsius`°C."
+let tempInCelsius = 37;
+let tempInFahrenheit = tempInCelsius * 9 / 5 + 32;
+console.log(`${tempInCelsius}°C is ${tempInFahrenheit}°F`);
+let tempInFahrenheit2 = 100;
+let tempInCelsius2 = (tempInFahrenheit2 - 32) * 5 / 9;
+console.log(`${tempInFahrenheit2}°F is ${tempInCelsius2}°C`);
+
+
 
 
 // ## Problem Five
@@ -49,7 +67,7 @@
 // Find the last number:
 // * You are given a number a. Print the last digit of a.
 // * Example
-// * Input: 
+// * Input:
 // * a = 123
 // * Output:
 // * 3
@@ -63,19 +81,19 @@
 // Alice's Age
 // * x years from now Alice will be y times older than her brother Bob. Bob is 12 years old. How old is Alice?
 // * Example 1
-// * Input: 
+// * Input:
 // * x = 3
 // * y = 2
 // * bob = 12
-// * Expected values: 
+// * Expected values:
 // * alice = 27
-// 
+//
 // * Example 2
-// * Input: 
+// * Input:
 // * x = 1
 // * y = 3
 // * bob = 12
-// * Expected values: 
+// * Expected values:
 // * alice = 38
 
 // * Hint:
@@ -87,9 +105,9 @@
 // * Cat and Dog Percentages
 // * An animal daycare consists of `numberOfCats` cats and `numberOfDogs` dogs.
 // * Print the percentage of dogs in the daycare followed by the percentage of cats in the class. The percentage should be printed rounded down to the nearest integer. For example 33.333333333333 will be printed as 33.
-// * Example 
+// * Example
 // * Input
-// * numberOfCats = 20  
+// * numberOfCats = 20
 // * numberOfDogs = 60
 // * Output:
 // * 25% of the daycare animals are cats
@@ -99,7 +117,7 @@
 
 // For this section write what you think will be logged as a comment next to `console.log` like so: `console.log('Cat') //'Cat'` before running the code. Then execute your file and compare with your prediction.
 
-// a. 
+// a.
 //     ```js
 //     let num1 = 2
 //     let num2 = 5
@@ -107,14 +125,14 @@
 //     console.log(num3)
 //     ```
 
-// b. 
+// b.
 //     ```js
-//     let str = 'jel' 
+//     let str = 'jel'
 //     str += 'lo'
 //     console.log(str)
 //     ```
 
-// c. 
+// c.
 //     ```js
 //     let string =  'My favorite number is ';
 //     let number = 42
@@ -125,12 +143,12 @@
 // ## Problem Ten (BONUS - PREVIEW)
 
 // * Leap Year Calculator
-// * Given a year, determine if it's a leap year.  
-// * A leap year is a year containing an extra day. It has 366 days instead of the normal 365 days. 
-// * The extra day is added in February, which has 29 days instead of the normal 28 days. 
-// * Leap years occur every 4 years. 2012 was a leap year and 2016 will also be a leap year. 
-// * The above rule is valid except that every 100 years special rules apply. 
-// * Years that are divisible by 100 are not leap years if they are not also divisible by 400. 
+// * Given a year, determine if it's a leap year.
+// * A leap year is a year containing an extra day. It has 366 days instead of the normal 365 days.
+// * The extra day is added in February, which has 29 days instead of the normal 28 days.
+// * Leap years occur every 4 years. 2012 was a leap year and 2016 will also be a leap year.
+// * The above rule is valid except that every 100 years special rules apply.
+// * Years that are divisible by 100 are not leap years if they are not also divisible by 400.
 // * For example 1900 was not a leap year, but 2000 was. Print "Leap year!" or "Not a leap year!" depending on the year you are provided.
 
 
